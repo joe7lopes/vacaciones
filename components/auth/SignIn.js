b8ba582ae622eba09e3334e86d5f8d9b1a58a68f
Joao Oliveira <j.andre.oliveira@gmail.com>
2018-03-19 20:50:35 +0100

    refactor login screen

--- components/auth/SignIn.js
+++ components/auth/SignIn.js
@@ -1,41 +1,75 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { COLOR_PRIMARY_LIGHTER, FONT_SIZE_LOGIN_BUTTON } from '../../styles/style';
+import { SafeAreaView, View, Text, Dimensions, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  COLOR_PRIMARY,
+  FONT_SIZE_TITLE,
+  FONT_SIZE_TEXT
+} from '../../styles/style';
 
 const SignIn = () => (
-  <View style={styles.container}>
-    <Text>Making Plans with your friends don't have to be a pain anymore</Text>
-    <TouchableOpacity style={styles.facebookButton}>
-      <Text style={styles.signInButtonText}>Facebook</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.googleButton}>
-      <Text style={styles.signInButtonText}>Google</Text>
-    </TouchableOpacity>
-  </View>
+  <SafeAreaView style={styles.main}>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.logo}> Vacaciones </Text>
+        <Text style={styles.loginText}>Making Plans with your friends don't have to be a pain anymore</Text>
+      </View>
+      <View style={styles.body}>
+        <TouchableOpacity style={styles.facebookButton}>
+          <Text style={styles.signInButtonText}>Facebook</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.googleButton}>
+          <Text style={styles.signInButtonText}>Google</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  </SafeAreaView>
 );
-
+const { width, height } = Dimensions.get('window');
 const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: COLOR_PRIMARY
+  },
   container: {
     flex: 1,
-    top: 100,
-    padding: 20
+    marginLeft: width / 20,
+    marginRight: width / 20,
+    marginBottom: width / 20,
+  },
+  header: {
+    flex: 1
+  },
+  body: {
+    flex: 1,
+    justifyContent: 'flex-start'
+  },
+  logo: {
+    marginTop: height / 20,
+    fontSize: FONT_SIZE_TITLE,
+    textAlign: 'center',
+    color: 'white',
+  },
+  loginText: {
+    marginTop: height / 20,
+    fontSize: FONT_SIZE_TEXT,
+    textAlign: 'center',
+    color: 'white'
   },
   facebookButton: {
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: COLOR_PRIMARY_LIGHTER
+    backgroundColor: 'blue',
+    padding: 10
   },
   googleButton: {
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'red'
+    marginTop: 5,
+    backgroundColor: 'red',
+    padding: 10
   },
   signInButtonText: {
-    fontSize: FONT_SIZE_LOGIN_BUTTON,
-    color: 'white'
+    fontSize: FONT_SIZE_TEXT,
+    color: 'white',
+    textAlign: 'center'
   }
+
 });
 
 export default SignIn;
--- styles/style.js
+++ styles/style.js
@@ -1,5 +1,5 @@
 /* colors */
-export const COLOR_PRIMARY = '#3F51B5';
+export const COLOR_PRIMARY = '#55efc4';
 export const COLOR_PRIMARY_LIGHTER = '#3F51B5';
 export const COLOR_PRIMARY_DARKER = '#303F9F';
 export const COLOR_SECONDARY = '#FF4081';
